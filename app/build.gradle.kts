f9e5f51744849ca9a53889c7ba418a2c3587ef36
Alessandro <alessrodriguez98@gmail.com>
2023-11-30 17:09:39 -0500

    update javax lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.applifycu"
             artifactId = "nautaclear"
-            version = "1.0.0"
+            version = "1.0.0-alpha2"
 
             afterEvaluate {
                 from(components["release"])
